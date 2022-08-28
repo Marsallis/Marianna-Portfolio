3b7282534ed662644432129cf6c0fddca7a90afe
marsallis faircloth <Marsallisf@gmail.com>
2022-08-28 16:40:28 -0400

    still fixing mp4 videos

--- src/components/PicVid.jsx
+++ src/components/PicVid.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "../Styles/PicVid.css";
-
+import video1 from "../assets/Videos/video1.mp4";
+import video2 from "../assets/Videos/video2.mp4";
+import video3 from "../assets/Videos/video3.mp4";
+import video4 from "../assets/Videos/video4.mp4";
+import video5 from "../assets/Videos/video5.mp4";
+import video6 from "../assets/Videos/video6.mp4";
+import video7 from "../assets/Videos/video7.mp4";
+import video8 from "../assets/Videos/video8.mp4";
 
 const PicVid = () => {
     return(
@@ -8,44 +15,44 @@ const PicVid = () => {
             <h2>PAST VIDEO PROJECT</h2>
             <div className="videos_section1">
                 <div className="video1">
-                    <video src="/Videos/video1.mp4" playsinline width="300" height="500" controls type="video/mp4">
+                    <video src={video1} playsinline width="300" height="500" controls type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video2">
-                    <video src="/Videos/video2.mp4" playsinline width="300" height="500"controls type="video/mp4">
+                    <video src={video2} playsinline width="300" height="500"controls type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video3">
-                    <video src="/Videos/video3.mp4" playsinline width="300" height="500"controls type="video/mp4">
+                    <video src={video3} playsinline width="300" height="500"controls type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video4">
-                    <video src="/Videos/video4.mp4" playsinline width="300" height="500"controls type="video/mp4">s
+                    <video src={video4} playsinline width="300" height="500"controls type="video/mp4">s
                     Your browser does not support the video tag.
                     </video>
                 </div>
             </div>
             <div className="videos_section2">
                 <div className="video5">
-                    <video src="/Videos/video5.mp4" playsinline width="300" height="500"controls type="video/mp4">
+                    <video src={video5} playsinline width="300" height="500"controls type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video6">
-                    <video src="/Videos/video6.mp4" playsinline width="300" height="500"controls type="video/mp4">
+                    <video src={video6} playsinline width="300" height="500"controls type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video7">
-                    <video src="/Videos/video7.mp4" playsinline width="300" height="500"controls type="video/mp4">
+                    <video src={video7} playsinline width="300" height="500"controls type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video8">
-                    <video src="/Videos/video8.mp4" playsinline width="300" height="500"controls type="video/mp4">
+                    <video src={video8} playsinline width="300" height="500"controls type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
