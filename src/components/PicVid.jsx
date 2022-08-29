adc4f3b1bad831384cf6f72944354f1ad7d541a6
marsallis faircloth <Marsallisf@gmail.com>
2022-08-29 16:39:10 -0400

    still fixing mp4 videos

--- src/components/PicVid.jsx
+++ src/components/PicVid.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "../Styles/PicVid.css";
 import video1 from "../assets/Videos/video1.mov";
+import video1w from "../assetsVideos/video1.webm";
 import video2 from "../assets/Videos/video2.mp4";
+import video2w from "../assetsVideos/video2.webm";
 import video3 from "../assets/Videos/video3.mp4";
+import video3w from "../assetsVideos/video3.webm";
 import video4 from "../assets/Videos/video4.mp4";
+import video4w from "../assetsVideos/video4.webm";
 import video5 from "../assets/Videos/video5.mp4";
+import video5w from "../assetsVideos/video5.webm";
 import video6 from "../assets/Videos/video6.mp4";
+import video6w from "../assetsVideos/video6.webm";
 import video7 from "../assets/Videos/video7.mp4";
+import video7w from "../assetsVideos/video7.webm";
 import video8 from "../assets/Videos/video8.mp4";
+import video8w from "../assetsVideos/video8.webm";
 
 const PicVid = () => {
     return(
@@ -15,50 +23,61 @@ const PicVid = () => {
             <h2>PAST VIDEO PROJECTS</h2>
             <div className="videos_section1">
                 <div className="video1">
-                    <video src={video1} width="300" height="500" autoPlay controls muted type="video/mov">
-                    Your browser does not support the video tag.
+                    <video width="300" height="500" autoPlay controls muted>
+                        <source src={video1}  type="video/mov"/>
+                        <source src={video1w} type="video/webm"/>
+                        Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video2">
-                    <video src={video2} width="300" height="500" autoPlay controls muted type="video/mp4">
-                    Your browser does not support the video tag.
+                    <video width="300" height="500" autoPlay controls muted>
+                        <source src={video2}  type="video/mov"/>
+                        <source src={video2w} type="video/webm"/>
+                        Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video3">
-                    <video src={video3} width="300" height="500" autoPlay controls muted type="video/mp4">
-                    Your browser does not support the video tag.
+                    <video width="300" height="500" autoPlay controls muted >
+                        <source src={video3}  type="video/mov"/>
+                        <source src={video3w} type="video/webm"/>
+                        Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video4">
-                    <video 
-                        src={video4} 
-                        width="300" 
-                        height="500" 
-                        autoPlay
-                        controls muted type="video/mp4">
+                    <video width="300" height="500" autoPlaycontrols muted>
+                        <source src={video4}  type="video/mov"/>
+                        <source src={video4w} type="video/webm"/>
                         Your browser does not support the video tag.
                     </video>
                 </div>
             </div>
             <div className="videos_section2">
                 <div className="video5">
-                    <video src={video5} width="300" height="500" autoPlay controls muted type="video/mp4">
-                    Your browser does not support the video tag.
+                    <video width="300" height="500" autoPlay controls muted >
+                        <source src={video5}  type="video/mov"/>
+                        <source src={video5w} type="video/webm"/>
+                        Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video6">
-                    <video src={video6} width="300" height="500" autoPlay controls muted type="video/mp4">
-                    Your browser does not support the video tag.
+                    <video width="300" height="500" autoPlay controls muted >
+                        <source src={video6}  type="video/mov"/>
+                        <source src={video6w} type="video/webm"/>
+                        Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video7">
-                    <video src={video7} width="300" height="500" autoPlay controls muted type="video/mp4">
-                    Your browser does not support the video tag.
+                    <video width="300" height="500" autoPlay controls muted >
+                        <source src={video7}  type="video/mov"/>
+                        <source src={video7w} type="video/webm"/>
+                        Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video8">
-                    <video src={video8} width="300" height="500" autoPlay controls muted type="video/mp4">
-                    Your browser does not support the video tag.
+                    <video width="300" height="500" autoPlay controls muted >
+                        <source src={video8}  type="video/mov"/>
+                        <source src={video8w} type="video/webm"/>
+                        Your browser does not support the video tag.
                     </video>
                 </div>
             </div>
