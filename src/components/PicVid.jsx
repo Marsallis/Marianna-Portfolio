3a44b82f75e18bacc565e3bffb6dfd25c61ef6fa
marsallis faircloth <Marsallisf@gmail.com>
2022-08-29 14:23:28 -0400

    still fixing mp4 videos

--- src/components/PicVid.jsx
+++ src/components/PicVid.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import "../Styles/PicVid.css";
+import video1 from "../assets/Videos/video1.mov";
+import video2 from "../assets/Videos/video2.mp4";
+import video3 from "../assets/Videos/video3.mp4";
+import video4 from "../assets/Videos/video4.mp4";
+import video5 from "../assets/Videos/video5.mp4";
+import video6 from "../assets/Videos/video6.mp4";
+import video7 from "../assets/Videos/video7.mp4";
+import video8 from "../assets/Videos/video8.mp4";
 
 const PicVid = () => {
     return(
@@ -7,24 +15,23 @@ const PicVid = () => {
             <h2>PAST VIDEO PROJECTS</h2>
             <div className="videos_section1">
                 <div className="video1">
-                    <video src="Videos/video1.mov" className="video1" width="300" height="500" controls muted type="video/mov">
+                    <video src={video1} width="300" height="500" controls muted type="video/mov">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video2">
-                    <video src="Videos/video2.mp4" className="video2" width="300" height="500"controls muted type="video/mp4">
+                    <video src={video2} width="300" height="500"controls muted type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video3">
-                    <video src="Videos/video3.mp4" className="video3" width="300" height="500"controls muted type="video/mp4">
+                    <video src={video3} width="300" height="500"controls muted type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video4">
                     <video 
-                        src="Videos/video4.mp4" 
-                        className="video4" 
+                        src={video4} 
                         width="300" 
                         height="500" 
                         controls muted type="video/mp4">
@@ -34,22 +41,22 @@ const PicVid = () => {
             </div>
             <div className="videos_section2">
                 <div className="video5">
-                    <video src="Videos/video5.mp4" className="video5"width="300" height="500"controls muted type="video/mp4">
+                    <video src={video5} width="300" height="500"controls muted type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video6">
-                    <video src="Videos/video6.mp4" className="video6" width="300" height="500"controls muted type="video/mp4">
+                    <video src={video6} width="300" height="500"controls muted type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video7">
-                    <video src="Videos/video7.mp4" className="video7" width="300" height="500"controls muted type="video/mp4">
+                    <video src={video7} width="300" height="500"controls muted type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
                 <div className="video8">
-                    <video src="Videos/video8.mp4" className="video8" width="300" height="500"controls muted type="video/mp4">
+                    <video src={video8} width="300" height="500"controls muted type="video/mp4">
                     Your browser does not support the video tag.
                     </video>
                 </div>
