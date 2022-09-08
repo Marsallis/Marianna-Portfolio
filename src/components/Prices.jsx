ccfa7d70d260eb84a788404b760fc39b2245d735
marsallis faircloth <Marsallisf@gmail.com>
2022-09-08 12:50:34 -0400

    added pricing page

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -14,49 +14,52 @@ const Prices = () => {
                 </div>
             </div>
             <div className="bundle_pricing">
-                <div>
+                <div className="bundle_box">
                     <h3>5 UGC Videos</h3>
                     <h3>+5 images</h3>
-                    <h3>$750</h3>
+                    <h4>$750</h4>
                 </div>
-                <div>
+                <div className="bundle_box">
                     <h3>10 UGC Videos</h3>
                     <h3>+5 images</h3>
-                    <h3>$1400</h3>
+                    <h4>$1400</h4>
                 </div>
-                <div>
+                <div className="bundle_box">
                     <h3>20 UGC Videos</h3>
                     <h3>+5 images</h3>
-                    <h3>$2400</h3>
+                    <h4>$2400</h4>
                 </div>
             </div>
             <div className="single_media_pricing">
                 <div>
                     <h3>UGC Video Prices</h3>
-                    <div>
+                    <div className="pricing_list">
                         <p>1 UGC VIDEO</p>
                         <p>$150</p>
                     </div>
-                    <div>
+                    <div className="pricing_list">
                         <p>3 UGC VIDEOS</p>
                         <p>$400</p>
                     </div>
-                    <div>
+                    <div className="pricing_list">
                         <p>5 UGC VIDEOS</p>
                         <p>$700</p>
                     </div>
                 </div>
                 <div>
                     <h3>PRODUCT PRICES</h3>
-                    <div>
+                    <div className="pricing_list">
                         <p>5 UGC PHOTOS</p>
                         <p>$100</p>
                     </div>
-                    <div>
+                    <div className="pricing_list">
                         <p>15 UGC VIDEOS</p>
                         <p>$220</p>
                     </div>
                 </div>
+            </div>
+            <div className="disclaimer">
+                <p>Other bundle options available. Please email for details!</p>
             </div>
         </div>
     )
