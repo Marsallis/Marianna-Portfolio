16de7f1c0ca6f093fabe161ad355cd54ee734123
marsallis faircloth <Marsallisf@gmail.com>
2022-09-09 13:31:53 -0400

    fixed some positioning on multiple elements

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -4,7 +4,7 @@ import "../Styles/Prices.css";
 
 const Prices = () => {
     return(
-        <div className="container">
+        <div className="pricing_container" id="pricing_container">
             <div className="pricing_header">
                 <div>
                     <h2>PRICING</h2>
