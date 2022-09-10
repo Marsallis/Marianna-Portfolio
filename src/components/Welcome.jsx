68935c80374354fa89823ce3dd3b7561835c7003
marsallis faircloth <Marsallisf@gmail.com>
2022-09-10 02:05:33 -0400

    removed unnec stuff

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,7 +10,7 @@ const Welcome = () => {
         <div className="Welcome">
             <div className="text">
                 <h2 className="special-text"><span>Are you looking for Authentic and Organic short video ads for your brand?</span></h2>
-                <h3 className="special-text-2"><span><a href="mailto:marianna.ugcmarketing@gmail.com" >Let's Chat!</a></span></h3>
+                <h3 className="special-text-2"><span><a href="mailto:marianna.ugcmarketing@gmail.com">Let's Chat!</a></span></h3>
             </div>
             <div className="video">
                 <img src={template} className="template" width="535" height="820" alt="template" class="silhouette"/>
