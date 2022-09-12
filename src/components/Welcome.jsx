46cdea7d0dff5191484b2654cf247e9b9d1aea1b
marsallis faircloth <Marsallisf@gmail.com>
2022-09-12 16:40:46 -0400

    working on videos working on ios

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -14,7 +14,7 @@ const Welcome = () => {
             </section>
             <section className="video">
                 <img src={template} className="template" width="535" height="820" alt="template" class="silhouette"/>
-                <video className="main_vid" width="455" height="950" alt="" autoPlay="autoPlay" muted loop="loop" defaultMuted playsInline onContextMenu="return false;" preload="auto">
+                <video className="main_vid" width="455" height="950" alt="" autoPlay muted loop="loop" defaultMuted playsInline onContextMenu="return false;" preload="metadata">
                     <source src={vid1} type="video/mp4"/>
                     <source src={video1} type="video/mp4"/>
                     <source src={video1w} type="video/webm"/>
