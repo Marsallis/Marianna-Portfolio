5fdb6190a092d3a6af291151f94f3b5327e467d3
marsallis faircloth <Marsallisf@gmail.com>
2022-09-12 16:49:30 -0400

    working on video posters

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -14,7 +14,7 @@ const Welcome = () => {
             </section>
             <section className="video">
                 <img src={template} className="template" width="535" height="820" alt="template" class="silhouette"/>
-                <video className="main_vid" width="455" height="950" alt="" autoPlay muted loop="loop" defaultMuted playsInline onContextMenu="return false;" preload="metadata">
+                <video className="main_vid" width="455" height="950" alt="" autoPlay muted loop="loop" defaultMuted playsInline onContextMenu="return false;" preload="autoplay">
                     <source src={vid1} type="video/mp4"/>
                     <source src={video1} type="video/mp4"/>
                     <source src={video1w} type="video/webm"/>
