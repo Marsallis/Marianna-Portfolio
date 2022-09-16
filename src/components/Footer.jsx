ec54b761918b95d6d71580156e281cfa39de0cf6
marsallis faircloth <Marsallisf@gmail.com>
2022-09-16 12:16:56 -0400

    necessary changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ import { AiFillCamera, AiFillMail } from 'react-icons/ai';
 const Footer = () => {
     return(
         <div className="footer-container">
-            <div className="props"><span>&#169;</span> Website Made By Faircloth Freelancing LLC</div> 
             <div className="icons">
                 <a href="https://www.instagram.com/marianna_ugc/" class="fa-ig"><AiFillCamera/></a>
                 <a href="mailto:marianna.ugcmarketing@gmail.com" class="fa-mail"><AiFillMail/></a>
             </div>
+            <div className="props"><span>&#169;</span> Website Made By Faircloth Freelancing LLC</div>
         </div>
     )
 }
